dec8d2463984a8508f53b80b4e78f28153c3756b
Brian Orr <Brian@dayznchill.com>
2022-08-27 00:02:28 -0500

    gamecore init

--- scripts/3_Game/constants.c
+++ scripts/3_Game/constants.c
@@ -5,26 +5,17 @@
 string GetRandomBackground()
 {
 	const string images[] = {
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-1.edds", 
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-2.edds",
-        "Colorful-UI/gui/textures/loading_screens/Colorful-BG-3.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-4.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-5.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-6.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-7.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-8.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-9.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-10.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-11.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-12.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-13.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-14.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-15.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-16.edds",
-		"Colorful-UI/gui/textures/loading_screens/Colorful-BG-17.edds"
+		"Colorful-UI/gui/textures/loading_screens/Loading_Screen_1.edds", 
+		"Colorful-UI/gui/textures/loading_screens/Loading_Screen_2.edds",
+		"Colorful-UI/gui/textures/loading_screens/Loading_Screen_3.edds",
+		"Colorful-UI/gui/textures/loading_screens/Loading_Screen_4.edds",
+		"Colorful-UI/gui/textures/loading_screens/Loading_Screen_5.edds",		
+		"Colorful-UI/gui/textures/loading_screens/Loading_Screen_6.edds",
+		"Colorful-UI/gui/textures/loading_screens/Loading_Screen_7.edds", 
+		"Colorful-UI/gui/textures/loading_screens/Loading_Screen_8.edds"
 	};
 	Math.Randomize(-1);
-	int bgIndex = Math.RandomInt(0, 100) % 17;
+	int bgIndex = Math.RandomInt(0, 100) % 8;
 	return images[bgIndex];
 }
 
