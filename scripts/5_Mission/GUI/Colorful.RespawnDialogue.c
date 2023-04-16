e22f087e7229ec178a410990faf0ead1ad4dcd1e
Brian Orr <brian@dayznchill.com>
2023-04-15 21:36:42 -0500

    I am to embarrassed to type here.

--- scripts/5_Mission/GUI/Colorful.RespawnDialogue.c
+++ scripts/5_Mission/GUI/Colorful.RespawnDialogue.c
@@ -10,7 +10,7 @@ modded class RespawnDialogue extends UIScriptedMenu
 
 	override Widget Init()
 	{
-		layoutRoot 					= GetGame().GetWorkspace().CreateWidgets("colorful-ui/gui/layouts/colorful.2.day_z_respawn_dialogue.layout");
+		layoutRoot 					= GetGame().GetWorkspace().CreateWidgets("colorful-ui/gui/layouts/colorful.day_z_respawn_dialogue.layout");
 		m_DetailsRoot 				= layoutRoot.FindAnyWidget( "menu_details_tooltip" );
 		m_DetailsLabel				= TextWidget.Cast( m_DetailsRoot.FindAnyWidget( "menu_details_label" ) );
 		m_DetailsText				= RichTextWidget.Cast( m_DetailsRoot.FindAnyWidget( "menu_details_tooltip_content" ) );
