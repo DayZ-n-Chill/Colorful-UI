6bb93aa18102521d13b539ba575b681b42aff005
DayZ n' Chill™ <brian@dayznchill.com>
2023-11-07 20:50:53 -0600

    Removed animatin test

--- scripts/3_Game/Coloful.DayZGame.c
+++ scripts/3_Game/Coloful.DayZGame.c
@@ -5,64 +5,9 @@ modded class LoginQueueBase extends UIScriptedMenu
 	protected TextWidget m_StatusText;
 	private	Widget m_shader
 
-	private	Widget m_TestAmnim1
-	private	Widget m_TestAmnim2
-	private	Widget m_TestAmnim3
-	private	Widget m_TestAmnim4
-	private	Widget m_TestAmnim5
-	private int m_NewAnimationCurrentFrame = 1; 
-	private bool m_NewAnimationDirection = true;
-	void LoadingIndicator() 
-	{
-    	m_TestAmnim1.Show(false);
-    	m_TestAmnim2.Show(false);
-    	m_TestAmnim3.Show(false);
-    	m_TestAmnim4.Show(false);
-    	m_TestAmnim5.Show(false);
-
-    	switch (m_NewAnimationCurrentFrame) {
-    	    case 1:
-    	        m_TestAmnim1.Show(true);
-    	        break;
-    	    case 2:
-    	        m_TestAmnim2.Show(true);
-    	        break;
-    	    case 3:
-    	        m_TestAmnim3.Show(true);
-    	        break;
-    	    case 4:
-    	        m_TestAmnim4.Show(true);
-    	        break;
-    	    case 5:
-    	        m_TestAmnim5.Show(true);
-    	        break;
-    	}
-
-   		if (m_NewAnimationDirection) {
-   		    m_NewAnimationCurrentFrame++;
-   		    if (m_NewAnimationCurrentFrame > 5) {
-   		        m_NewAnimationCurrentFrame = 4; 
-   		        m_NewAnimationDirection = false;
-   		    }
-   		} else {
-   		    m_NewAnimationCurrentFrame--;
-   		    if (m_NewAnimationCurrentFrame < 1) {
-   		        m_NewAnimationCurrentFrame = 2; 
-   		        m_NewAnimationDirection = true; 
-   		    }
-   		}
-
-    	GetGame().GetCallQueue(CALL_CATEGORY_GUI).CallLater(this.LoadingIndicator, 150, false);
-	}
-
 	override Widget Init()
 	{		
 		layoutRoot 			   = GetGame().GetWorkspace().CreateWidgets("Colorful-UI/gui/layouts/Colorful.dialog_queue_position.layout");
-		m_TestAmnim1				= layoutRoot.FindAnyWidget( "TestAmnim1" );
-		m_TestAmnim2				= layoutRoot.FindAnyWidget( "TestAmnim2" );
-		m_TestAmnim3				= layoutRoot.FindAnyWidget( "TestAmnim3" );
-		m_TestAmnim4				= layoutRoot.FindAnyWidget( "TestAmnim4" );
-		m_TestAmnim5				= layoutRoot.FindAnyWidget( "TestAmnim5" );
 		m_txtPosition		   = TextWidget.Cast(layoutRoot.FindAnyWidget("txtPosition"));
 		m_txtNote 			   = TextWidget.Cast(layoutRoot.FindAnyWidget("txtNote"));
 		m_btnLeave 			   = ButtonWidget.Cast(layoutRoot.FindAnyWidget("btnLeave"));
@@ -74,7 +19,6 @@ modded class LoginQueueBase extends UIScriptedMenu
 		layoutRoot.FindAnyWidget("notification_root").Show(false);
 		m_ProgressLoading.SetColor(colorScheme.LoadingbarColor());
 		m_StatusText.SetColor(colorScheme.SecondaryText());
-		LoadingIndicator();
 		return layoutRoot;
 	}
 };
@@ -84,65 +28,10 @@ modded class LoginTimeBase extends UIScriptedMenu
 	ProgressBarWidget m_ProgressLoading;
 	protected TextWidget m_TxtHdr;
 	private	Widget m_shader
-
-	private	Widget m_TestAmnim1
-	private	Widget m_TestAmnim2
-	private	Widget m_TestAmnim3
-	private	Widget m_TestAmnim4
-	private	Widget m_TestAmnim5
-	private int m_NewAnimationCurrentFrame = 1; 
-	private bool m_NewAnimationDirection = true;
-	void LoadingIndicator() 
-	{
-    	m_TestAmnim1.Show(false);
-    	m_TestAmnim2.Show(false);
-    	m_TestAmnim3.Show(false);
-    	m_TestAmnim4.Show(false);
-    	m_TestAmnim5.Show(false);
-
-    	switch (m_NewAnimationCurrentFrame) {
-    	    case 1:
-    	        m_TestAmnim1.Show(true);
-    	        break;
-    	    case 2:
-    	        m_TestAmnim2.Show(true);
-    	        break;
-    	    case 3:
-    	        m_TestAmnim3.Show(true);
-    	        break;
-    	    case 4:
-    	        m_TestAmnim4.Show(true);
-    	        break;
-    	    case 5:
-    	        m_TestAmnim5.Show(true);
-    	        break;
-    	}
-
-   		if (m_NewAnimationDirection) {
-   		    m_NewAnimationCurrentFrame++;
-   		    if (m_NewAnimationCurrentFrame > 5) {
-   		        m_NewAnimationCurrentFrame = 4; 
-   		        m_NewAnimationDirection = false;
-   		    }
-   		} else {
-   		    m_NewAnimationCurrentFrame--;
-   		    if (m_NewAnimationCurrentFrame < 1) {
-   		        m_NewAnimationCurrentFrame = 2; 
-   		        m_NewAnimationDirection = true; 
-   		    }
-   		}
-
-    	GetGame().GetCallQueue(CALL_CATEGORY_GUI).CallLater(this.LoadingIndicator, 150, false);
-	}
 	
 	override Widget Init()
 	{
 		layoutRoot 			   = GetGame().GetWorkspace().CreateWidgets("Colorful-UI/gui/layouts/Colorful.dialog_login_time.layout");
-		m_TestAmnim1				= layoutRoot.FindAnyWidget( "TestAmnim1" );
-		m_TestAmnim2				= layoutRoot.FindAnyWidget( "TestAmnim2" );
-		m_TestAmnim3				= layoutRoot.FindAnyWidget( "TestAmnim3" );
-		m_TestAmnim4				= layoutRoot.FindAnyWidget( "TestAmnim4" );
-		m_TestAmnim5				= layoutRoot.FindAnyWidget( "TestAmnim5" );
 		m_TxtHdr			   = TextWidget.Cast(layoutRoot.FindAnyWidget("TxtHdr"));
 		m_txtLabel 			   = TextWidget.Cast(layoutRoot.FindAnyWidget("txtLabel"));
 		m_txtDescription 	   = TextWidget.Cast(layoutRoot.FindAnyWidget("txtDescription"));
@@ -154,7 +43,6 @@ modded class LoginTimeBase extends UIScriptedMenu
 		m_ProgressLoading.SetColor(colorScheme.LoadingbarColor());
 		m_TxtHdr.SetColor(colorScheme.SecondaryText());
         Background.LoadImageFile(0, GetRandomBackground());  		
-		LoadingIndicator();
 		return layoutRoot;
 	}
 
@@ -195,65 +83,10 @@ modded class LoadingScreen
 	protected TextWidget m_ProTip;
 	private	Widget m_shader
 
-	private	Widget m_TestAmnim1
-	private	Widget m_TestAmnim2
-	private	Widget m_TestAmnim3
-	private	Widget m_TestAmnim4
-	private	Widget m_TestAmnim5
-	private int m_NewAnimationCurrentFrame = 1; 
-	private bool m_NewAnimationDirection = true;
-	void LoadingIndicator() 
-	{
-    	m_TestAmnim1.Show(false);
-    	m_TestAmnim2.Show(false);
-    	m_TestAmnim3.Show(false);
-    	m_TestAmnim4.Show(false);
-    	m_TestAmnim5.Show(false);
-
-    	switch (m_NewAnimationCurrentFrame) {
-    	    case 1:
-    	        m_TestAmnim1.Show(true);
-    	        break;
-    	    case 2:
-    	        m_TestAmnim2.Show(true);
-    	        break;
-    	    case 3:
-    	        m_TestAmnim3.Show(true);
-    	        break;
-    	    case 4:
-    	        m_TestAmnim4.Show(true);
-    	        break;
-    	    case 5:
-    	        m_TestAmnim5.Show(true);
-    	        break;
-    	}
-
-   		if (m_NewAnimationDirection) {
-   		    m_NewAnimationCurrentFrame++;
-   		    if (m_NewAnimationCurrentFrame > 5) {
-   		        m_NewAnimationCurrentFrame = 4; 
-   		        m_NewAnimationDirection = false;
-   		    }
-   		} else {
-   		    m_NewAnimationCurrentFrame--;
-   		    if (m_NewAnimationCurrentFrame < 1) {
-   		        m_NewAnimationCurrentFrame = 2; 
-   		        m_NewAnimationDirection = true; 
-   		    }
-   		}
-
-    	GetGame().GetCallQueue(CALL_CATEGORY_GUI).CallLater(this.LoadingIndicator, 150, false);
-	}
-
 	void LoadingScreen(DayZGame game)
 	{
 		m_DayZGame   = game;		
 		m_WidgetRoot = game.GetLoadingWorkspace().CreateWidgets("Colorful-UI/gui/layouts/Colorful.loading.layout");
-		m_TestAmnim1				= m_WidgetRoot.FindAnyWidget( "TestAmnim1" );
-		m_TestAmnim2				= m_WidgetRoot.FindAnyWidget( "TestAmnim2" );
-		m_TestAmnim3				= m_WidgetRoot.FindAnyWidget( "TestAmnim3" );
-		m_TestAmnim4				= m_WidgetRoot.FindAnyWidget( "TestAmnim4" );
-		m_TestAmnim5				= m_WidgetRoot.FindAnyWidget( "TestAmnim5" );
 		Class.CastTo(m_ColorfulLogo, m_WidgetRoot.FindAnyWidget("ColorfulLogo"));
 		Class.CastTo(m_ProTip, m_WidgetRoot.FindAnyWidget("ProTip"));
 		Class.CastTo(m_shader, m_WidgetRoot.FindAnyWidget("Colorful_Shader"));
@@ -271,7 +104,6 @@ modded class LoadingScreen
 		m_shader.SetColor(colorScheme.ShaderColor());
 		ProgressAsync.SetProgressData(m_ProgressLoading);
 		ProgressAsync.SetUserData(m_ImageBackground);
-		LoadingIndicator();
 		SetStatus(m_TextWidgetStatus.GetRandomHint());
 	
 	}	
