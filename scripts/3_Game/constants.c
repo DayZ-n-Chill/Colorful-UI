ad85aad5c2722e83d747fa080c2cd6ed62db1035
Brian Orr <DrkDevil@users.noreply.github.com>
2023-12-01 13:07:17 -0600

    Some requested fixes

--- scripts/3_Game/constants.c
+++ scripts/3_Game/constants.c
@@ -6,14 +6,14 @@ static bool m_ShowDeathScreen = true;
 string GetRandomBackground()
 {
 	const string images[] = {
-		"Colorful-UI/gui/textures/loading_screens/LabLoadScreen_1.edds", 
-		"Colorful-UI/gui/textures/loading_screens/LabLoadScreen_2.edds",
-        "Colorful-UI/gui/textures/loading_screens/LabLoadScreen_3.edds",
-		"Colorful-UI/gui/textures/loading_screens/LabLoadScreen_4.edds",
-		"Colorful-UI/gui/textures/loading_screens/LabLoadScreen_5.edds",
-		"Colorful-UI/gui/textures/loading_screens/LabLoadScreen_6.edds",
-		"Colorful-UI/gui/textures/loading_screens/LabLoadScreen_7.edds",
-		"Colorful-UI/gui/textures/loading_screens/LabLoadScreen_8.edds"
+		"Colorful-UI/gui/textures/loading_screens/Colorful-UI-BG-1.edds", 
+		"Colorful-UI/gui/textures/loading_screens/Colorful-UI-BG-2.edds",
+        "Colorful-UI/gui/textures/loading_screens/Colorful-UI-BG-3.edds",
+		"Colorful-UI/gui/textures/loading_screens/Colorful-UI-BG-4.edds",
+		"Colorful-UI/gui/textures/loading_screens/Colorful-UI-BG-5.edds",
+		"Colorful-UI/gui/textures/loading_screens/Colorful-UI-BG-6.edds",
+		"Colorful-UI/gui/textures/loading_screens/Colorful-UI-BG-7.edds",
+		"Colorful-UI/gui/textures/loading_screens/Colorful-UI-BG-8.edds"
 	};
 	const int IMAGES_COUNT = 8;
 	int bgIndex = Math.RandomInt(0, IMAGES_COUNT - 1);
@@ -50,29 +50,59 @@ class MenuURLS {
 string GetRandomHint()
 {
 	const string tips[] = {
-		"Death makes you die.",
-		"Broomsticks give you enough resources to make a fire place and cooking stand.",
-		"Keep your car running smoothby using min 4 water bottles to fill the radiator.",
-		"You can craft a bone knife with one bone if you use rocks or boulders to craft it.",
-		"Try to blend in with your enviornment. Do not wear bright colors",
-		"If someone is harrassing you contact a server admin immediately",
-		"Non aggro infected will detect you in 2 seconds when sneaking up on them. Stealth kill them quick",
-		"Craft foot wraps to dampen the sound of your movement.",
-		"Breaking down ghillie parts you find is a faster way to get all the parts you need to make a full one.",
-		"Baking food provides the most nutritional value when cooking.",
-		"The flare gun can not kill anything in the game but yourself.",
-		"Armor Piercing rounds do not do extra damage, they just reduce the durability of armor faster.",
-		"Going unconsious in water will kill you in 20 seconds.",
-		"Having full white stats, or taking multivitamins will allow you to eat raw meat, drink from dirty water sources, and consume with bloody hands.",
-		"Apart from the Famas, all sawed off weapons have twice the ammount of recoil",
-		"Any weapon with a suppressor cannot be heard after 150 meters.",
-		"If you log into a dynamic gas zone you will be teleported out of the area.",
-		"If you are K.O. by a gunman you will always wake up facing the opposite the direction from where you were shot. ",
-		"Spike helmet only spawns at the Castle south of Sitnik on the red trail.",
-		"Remebering Paulie.  Loc. 11930 x 13157 - R.I.P"
+		"In the aftermath of the USSR's collapse, shadows of a new world emerged.",
+		"Atlas Industries rose in the chaos, shaping the fate of Chernarus.",
+		"Chernarus, scarred by civil war in 2009, faced an unforeseen epidemic.",
+		"Cancer cases soared, affecting both young and old.",
+		"Atlas and Chernarussian government collaborated to combat the crisis.",
+		"In 2013, the northern reaches became Atlas' enigmatic headquarters.",
+		"Scientific experiments took a dark turn, altering the course of history.",
+		"A hidden project re-emerged, its purpose far more sinister than before.",
+		"Survivors navigate a world tainted by the remnants of Atlas' ambitions.",
+		"The threat of infection looms, an unintended consequence of past actions.",
+		"Towns fall one by one as the aftermath of Atlas' experiments unfolds.",
+		"Chernarus, once familiar, now a landscape haunted by the ghosts of the past.",
+		"Moral choices shape alliances in a world governed by survival instincts.",
+		"The infected roam, a constant reminder of humanity's darker tendencies.",
+		"Resources dwindle as survivors face the aftermath of Atlas' legacy.",
+		"Hidden secrets within Atlas' labs hold both danger and potential salvation.",
+		"Echoes of the past reverberate through the ruins of once-thriving cities.",
+		"Survivors grapple with the blurred lines between friend and foe.",
+		"Atlas' experiments unleashed horrors that now roam the wastelands.",
+		"In the post-apocalyptic chaos, whispers of a cure become a beacon of hope.",
+		"Factions rise, each with their own motives in the struggle for survival.",
+		"The infected, once human, now part of a relentless force.",
+		"Atlas' downfall warns of the consequences of tampering with the unknown.",
+		"Trust becomes a rare commodity as survivors navigate the ruins.",
+		"The shattered remnants of civilization hold the key to a new beginning.",
+		"In the heart of darkness, remnants of Atlas pose both threat and opportunity.",
+		"The world crumbled, and survivors seek answers in the remnants of Atlas.",
+		"Atlas' experiments pushed the limits of science, leaving a legacy of fear.",
+		"Chernarus, a nation transformed, now faces the consequences of ambition.",
+		"As the infected grow in number, survivors face an uphill battle for survival.",
+		"The legacy shapes the struggle for dominance in the wasteland.",
+		"In the fog of war, alliances shift and crumble in the fight for resources.",
+		"The infected exhibit terrifying resilience, challenging the survivors' mettle.",
+		"Atlas' underground labs remain as cryptic puzzles waiting to be unraveled.",
+		"Whispers of the past guide survivors through the uncertain present.",
+		"Survivors grapple with the consequences of Atlas' experiments on the world.",
+		"Atlas' dark legacy is a constant reminder in the midst of the wasteland.",
+		"In the search for answers, survivors uncover the mysteries within Northern Chernarus.",
+		"The aftermath of Atlas' actions shapes the survivors' journey through chaos.",
+		"In the struggle for survival, remnants of Atlas pose a constant threat.",
+		"The infected, driven by the unknown, exhibit an eerie tenacity.",
+		"Atlas' experiments blur the line between science and madness in Chernarus.",
+		"As the world crumbles, survivors must confront the sins of the past.",
+		"The ruins tell tales of a Chernarus forever changed by Atlas' ambitions.",
+		"Echoes of Atlas' experiments linger in the silent corners of the wasteland.",
+		"Moral dilemmas shape the survivors' journey through a world undone.",
+		"Atlas' downfall becomes a cautionary tale in the survivors' struggle.",
+		"Chernarus, once a nation, now a canvas painted with the aftermath.",
+		"Remnants pose a constant threat in the wasteland.",
+		"Atlas' experiments forge a path through the dark corridors of Northern Chernarus."
 	};
 	Math.Randomize(-1);
-	int tipIndex = Math.RandomInt(0, 100) % 20;
+	int tipIndex = Math.RandomInt(0, 100) % 48;
 	return tips[tipIndex];
 }
 
